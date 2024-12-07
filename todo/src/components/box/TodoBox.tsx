eda7120ad0ab5549e124f0b4a7aa6f901ab5c970
RoqueLovelace <chrisdesanfrancisco@gmail.com>
2024-12-07 16:50:07 -0600

    Se añadió toda la funcionalidad. Falta modificar estilos

--- todo/src/components/box/TodoBox.tsx
+++ todo/src/components/box/TodoBox.tsx
@@ -2,35 +2,102 @@ import React, { useState } from "react";
 import GenericTextInput from "../input/field/GenericTextInput";
 import GenericCheckBox from "../input/checkbox/GenericCheckBox";
 const TodoBox = () => {
-  interface Tarea {
-    tarea: JSX.Element;
-  }
+  const [tareas, setTareas] = useState<{ titulo: string; completado: boolean; index: number }[]>([]);
+  const [tareasCompletadas, setTareasCompletadas] = useState<{ titulo: string; completado: boolean; index: number }[]>([]);
+  const [titulo, setTitulo] = useState<string>("");
 
-  const [tareas, setTareas] = useState<Tarea[]>([]);
+  const handlerAgregarTarea = () => {
+    if (titulo.trim() === "") return; // previene agregar tareas vacías
+
+    setTareas([{ titulo: titulo, completado: false, index: tareas.length }, ...tareas]);
+    setTitulo(""); // limpia el input
+  };
+  const handlerCambiarEstadoTarea = (index: number, state: boolean) => {
+    console.log('Soy la tarea ' + index + ' y ahora soy ' + state);
+
+    if (state) {
+      const tareaCompletadaIndex = tareasCompletadas.findIndex(tarea => tarea.index === index);
+
+      console.log('index de la tarea ',);
+      const dondeInsertar = tareas.findIndex(tarea => {
+        if (tarea.index < index) {
+          return true;
+        }
+      });
+
+      const newTareas = [...tareas];
+
+      const newTarea = tareasCompletadas[tareaCompletadaIndex];
+      newTarea.completado = false;
+
+      newTareas.splice(dondeInsertar, 0, newTarea);
+
+      setTareas(newTareas);
+
+
+      setTareasCompletadas(tareasCompletadas.filter((_, i) => i !== tareaCompletadaIndex));
 
 
 
-  const handlerAgregarTarea = (_title: string, _completado: boolean) => {
 
-    console.log(_title, _completado);
+    } else {
+      const tareaIndex = tareas.findIndex(tarea => {
+        if (tarea.index === index) {
+          return true;
+        }
+      })
+      const newTareasCompletada = { ...tareas[tareaIndex], completado: true };
+      setTareasCompletadas([newTareasCompletada, ...tareasCompletadas]);
+      setTareas(tareas.filter((_, i) => i !== tareaIndex));
 
-    const tarea = <GenericCheckBox titulo={_title} completado={_completado} />;
 
-    setTareas([...tareas, { tarea }]);
+    }
   };
 
+  const HayTareasCompletadas = () => {
+    if (tareasCompletadas.length === 0) return;
+    else {
+      return (
+        <>
+          <div className="hr-with-text">
+            <span>Tareas completadas</span>
+          </div>
+          {
+            tareasCompletadas.map((tarea, index) => (
+              <GenericCheckBox
+                key={index}
+                cbIndex={tarea.index}
+                titulo={tarea.titulo}
+                completado={tarea.completado}
+                handlerCompletarTarea={handlerCambiarEstadoTarea}
+              />
+            ))
+          }
+        </>
+
+      );
+    }
+  }
   return (
     <>
-      <GenericTextInput onClick={handlerAgregarTarea} />
+      <GenericTextInput
+        titulo={titulo}
+        onTituloChange={setTitulo}
+        onClick={handlerAgregarTarea} />
 
       {
-        tareas.map((tarea, index) => {
-          return (
-            <div key={index}>
-              {tarea.tarea}
-            </div>
-          );
-        })
+        tareas.map((tarea, index) => (
+          <GenericCheckBox
+            key={index}
+            cbIndex={tarea.index}
+            titulo={tarea.titulo}
+            completado={tarea.completado}
+            handlerCompletarTarea={handlerCambiarEstadoTarea}
+          />
+        ))
+      }
+      {
+        HayTareasCompletadas()
       }
     </>
   );
--- todo/src/components/input/checkbox/GenericCheckBox.tsx
+++ todo/src/components/input/checkbox/GenericCheckBox.tsx
@@ -1,23 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
 import classNames from 'classnames';
+
 interface GenericCheckBoxProps {
+  cbIndex: number;
   titulo: string;
   completado: boolean;
+  handlerCompletarTarea: (index: number, state: boolean) => void;
 }
-const GenericCheckBox: React.FC<GenericCheckBoxProps> = ({ titulo, completado = false }) => {
 
-  const [checked, setChecked] = useState(completado);
-
+const GenericCheckBox: React.FC<GenericCheckBoxProps> = ({ titulo, completado, handlerCompletarTarea, cbIndex }) => {
   const handleChange = () => {
-    setChecked(prevChecked => !prevChecked);
+    handlerCompletarTarea(cbIndex, completado);
   };
 
   return (
-    <div className={classNames('flex items-center m-5', { tachado: checked })}>
+    <div id={cbIndex.toString()} className={classNames('flex items-center m-5', { tachado: completado })}>
       <input
         onChange={handleChange}
         type="checkbox"
-        checked={checked}
+        checked={completado}
         className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
       />
       <label htmlFor="default-checkbox" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
