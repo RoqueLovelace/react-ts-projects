f2adda4b61e0c23270bcc4c147801f36b80c5c02
RoqueLovelace <chrisdesanfrancisco@gmail.com>
2024-12-07 19:28:29 -0600

    Se mejoraron los estilos

--- todo/src/components/box/NeonBox.tsx
+++ todo/src/components/box/NeonBox.tsx
@@ -8,7 +8,7 @@ interface MyComponentProps {
 const NeonContainer = ({ children }: MyComponentProps) => {
   return (
     <div
-      className="flex-col justify-center items-center text-sky-200 border-2 rounded-lg border-sky-200 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f]"
+      className="todo flex-col text-sky-200 border-2 rounded-lg border-sky-200 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f]"
     >
       {children}
     </div>
